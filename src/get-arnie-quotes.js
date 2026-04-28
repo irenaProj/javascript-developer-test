6cdadf9a549af111e4e8dcdcc8c53fef51cdf817
irenaProj <irenaProj@users.noreply.github.com>
2026-04-28 04:31:06 +0000

    feat: implement getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,26 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+const getSingleQuote = async (url) => {
+  const response = await httpGet(url);
+  const { message } = JSON.parse(response.body);
+
+  if (response.status !== 200) {
+    throw new Error(message);
+  }
+
+  return message;
+};
+
+const toResult = (fetchResult) =>
+  fetchResult.status === "fulfilled"
+    ? { "Arnie Quote": fetchResult.value }
+    : { FAILURE: fetchResult.reason.message };
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const fetchResults = await Promise.allSettled(urls.map(getSingleQuote));
+  const results = fetchResults.map(toResult);
+
+  return results;
 };
 
 module.exports = {
